12b85239bbbc65662874328a5c01b42fc49241e4
chano <chano22@naver.com>
2019-07-12 21:39:47 +0900

    pcotest

---
+++ src/main/java/com/security/SecurityAndChartApplication.java
@@ -0,0 +1,13 @@
+package com.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityAndChartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityAndChartApplication.class, args);
+	}
+
+}
