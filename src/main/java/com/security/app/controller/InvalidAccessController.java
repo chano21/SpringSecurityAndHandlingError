eabb59ec30f2584fbc57119d150231b69cffda6c
chano22@naver.com <chano22@naver.com>
2019-07-16 20:27:33 +0900

    error handling clear

--- src/main/java/com/security/app/controller/InvalidAccessController.java
+++ src/main/java/com/security/app/controller/InvalidAccessController.java
@@ -1,51 +1,41 @@
-package com.security.app.controller;
-
-import java.util.Arrays;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.security.app.repositorys.MemberRepository;
-import com.security.app.tables.Member;
-import com.security.app.tables.MemberRole;
-
-@Controller
-@RequestMapping("/error")
-public class InvalidAccessController implements ErrorController {
-	
-	@RequestMapping("/")
-	public String handleError(HttpServletRequest request) {
-	    Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-	     
-	    System.out.println(status.toString());
-	    if (status != null) {
-	        Integer statusCode = Integer.valueOf(status.toString());
-	     
-	        if(statusCode == HttpStatus.NOT_FOUND.value()) {
-	            return "error-404";
-	        }
-	        else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-	            return "error-500";
-	        }
-	    }
-	    return "error";
-	}
-	
-	
-	@Override
-	public String getErrorPath() {
-		System.out.println("ㅎㅇ?");
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-}
+/*
+ * package com.security.app.controller;
+ * 
+ * import java.util.Arrays;
+ * 
+ * import javax.servlet.RequestDispatcher; import
+ * javax.servlet.http.HttpServletRequest;
+ * 
+ * import org.springframework.beans.factory.annotation.Autowired; import
+ * org.springframework.boot.web.servlet.error.ErrorController; import
+ * org.springframework.http.HttpStatus; import
+ * org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder; import
+ * org.springframework.stereotype.Controller; import
+ * org.springframework.web.bind.annotation.GetMapping; import
+ * org.springframework.web.bind.annotation.PostMapping; import
+ * org.springframework.web.bind.annotation.RequestMapping;
+ * 
+ * import com.security.app.repositorys.MemberRepository; import
+ * com.security.app.tables.Member; import com.security.app.tables.MemberRole;
+ * 
+ * @Controller
+ * 
+ * @RequestMapping("/error") public class InvalidAccessController implements
+ * ErrorController {
+ * 
+ * @RequestMapping("/") public String handleError(HttpServletRequest request) {
+ * Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+ * 
+ * System.out.println(status.toString()); if (status != null) { Integer
+ * statusCode = Integer.valueOf(status.toString());
+ * 
+ * if(statusCode == HttpStatus.NOT_FOUND.value()) { return "error-404"; } else
+ * if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) { return
+ * "error-500"; } } return "error"; }
+ * 
+ * 
+ * @Override public String getErrorPath() { System.out.println("ㅎㅇ?"); // TODO
+ * Auto-generated method stub return null; }
+ * 
+ * }
+ */
